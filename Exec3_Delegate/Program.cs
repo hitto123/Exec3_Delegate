c6d98512d63b997f8d3554196b38201e69fa4b50
ispan <ispan@DESKTOP-PAKSETB>
2022-11-09 11:56:57 +0800

    加入專案檔案。

---
+++ Exec3_Delegate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec3_Delegate
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> items = new List<int> { 8, 15, 26, 32, 35, 41, 100, 111 };
+			Func<int, bool> isEven = n => n % 2 == 0;
+
+			List<int> EvenItems = GetEvenItems(items, isEven);
+			foreach (var item in EvenItems)
+			{
+				Console.WriteLine(item);
+			}
+			
+			
+
+
+
+		}
+	
+		static List<int> GetEvenItems(List<int> source, Func<int, bool> func)
+		{
+			var result = new List<int>();
+
+			foreach (int item in source)
+			{
+				if (func(item) == true)
+				{
+					result.Add(item);
+				}
+			}
+
+			return result;
+		}
+	}
+	
+}
